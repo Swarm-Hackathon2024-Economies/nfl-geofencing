c2fcd5d718b8639e93d20a01eff9f7e85a13fc79
Naoya Hashidate <naoya_hashidate@mail.toyota.co.jp>
2024-10-08 20:20:48 +0900

    initial commit
    Co-authored-by: Shion Koga <shion_koga@mail.toyota.co.jp>

---
+++ nfl-geofencing/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  nfl-geofencing
+//
+//  Created by edamame on 2024/10/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ nfl-geofencing/nfl_geofencingApp.swift
@@ -0,0 +1,17 @@
+//
+//  nfl_geofencingApp.swift
+//  nfl-geofencing
+//
+//  Created by edamame on 2024/10/08.
+//
+
+import SwiftUI
+
+@main
+struct nfl_geofencingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
