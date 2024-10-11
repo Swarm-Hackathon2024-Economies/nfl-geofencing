1f120ce37934d7b367060a52085d804e2bc7693d
Keisuke-Isaji <sajiemon.999@gmail.com>
2024-10-12 01:50:55 +0900

    経路検索実装途中

--- nfl-geofencing/Drive/DriveScreen.swift
+++ nfl-geofencing/Drive/DriveScreen.swift
@@ -12,52 +12,14 @@ struct DriveScreen: View {
     var body: some View {
         NavigationStack{
             VStack {
-                HStack {
-                    Button("行先") {
-                        self.currentPage = page.destination
-                    }
-                    Button("リスト") {
-                        self.currentPage = page.list
-                    }
-                }
-                switch currentPage {
-                case .destination:
-                    VStack {
-                        RouteDetailView()
-                        Spacer()
-                    }
-                case .list:
-                    List{
-                        NavigationLink {
-                            RouteDetailView()
-                        } label: {
-                            Text("ハワイ")
-                        }
-                        NavigationLink {
-                            RouteDetailView()
-                        } label: {
-                            Text("テキサス")
-                        }
-                        NavigationLink {
-                            RouteDetailView()
-                        } label: {
-                            Text("ネバダ")
-                        }
-                    }
-                    .navigationBarItems(
-                        trailing: Button(action: {
-                            self.isShowAddSheet = true
-                        }) {
-                            Image(systemName: "plus")
-                        }
-                    )
-                }
+                RouteDetailView()
+                Spacer()
             }
-            .sheet(isPresented: $isShowAddSheet) {
-                DestinationRegistorView(onSubmit: {
-                    isShowAddSheet = false
-                })
-            }
+        }
+        .sheet(isPresented: $isShowAddSheet) {
+            DestinationRegistorView(onSubmit: {
+                isShowAddSheet = false
+            })
         }
     }
 }
--- nfl-geofencing/RouteDetailView.swift
+++ nfl-geofencing/RouteDetailView.swift
@@ -4,45 +4,160 @@ import MapKit
 struct RouteDetailView: View {
     @State private var destinationInputText = ""
     @State private var arrivalInputText = ""
-    @State private var route: MKRoute?
-    @State private var isShowingRoutes = false
+    @State private var routes: [MKRoute]?
+    @State private var modalOffset: CGFloat = UIScreen.main.bounds.height - 125
+    @State private var lastModalOffset: CGFloat = UIScreen.main.bounds.height - 125
+    @State var isOverlayVisible: Bool = true
+
 
     var body: some View {
-        VStack {
-            HStack{
-                Text("出発地")
-                TextField("出発地", text:$destinationInputText)
-            }
-            HStack{
-                Text("到着地")
-                TextField("到着地", text:$arrivalInputText)
-            }
-            Map() {
-                if let routePolyline = route?.polyline {
-                    MapPolyline(routePolyline)
-                        .stroke(.blue, lineWidth: 8)
+        ZStack(alignment: .bottom) {
+            VStack {
+                Map {
+                    if let unwrappedRoutes = routes {
+                        ForEach(unwrappedRoutes, id: \.self) { route in
+                            if let routePolyline = route.polyline as? MKPolyline {
+                                MapPolyline(routePolyline)
+                                    .stroke(Color.blue, lineWidth: 8)
+                            }
+                        }
+                    }
+                }
+                .onAppear {
+                    Task {
+                        await calculateRoute()
+                    }
                 }
             }
-            .onAppear {
-                Task {
-                    await calculateRoute()
+            VStack {
+                ScrollView(.vertical, showsIndicators: false) {
+                    Spacer()
+                    VStack {
+                        Capsule()
+                            .frame(width: 40, height: 5)
+                            .foregroundColor(.gray)
+                            .padding(.top, 8)
+                            .onTapGesture {
+                                withAnimation {
+                                    if modalOffset == UIScreen.main.bounds.height - 555 {
+                                        modalOffset = UIScreen.main.bounds.height - 125
+                                    } else {
+                                        modalOffset = UIScreen.main.bounds.height - 555
+                                    }
+                                    lastModalOffset = modalOffset
+                                }
+                            }
+                        HStack(spacing:0) {
+                            Image(systemName: "book.pages")
+                            Text("Playbook")
+                                .font(.title2)
+                                .bold()
+                            Spacer()
+                        }
+                        VStack {
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color("BackgroundColor"))
+                                VStack {
+                                    HStack{
+                                        Text("出発地")
+                                        TextField("出発地", text:$destinationInputText)
+                                    }
+                                    HStack{
+                                        Text("到着地")
+                                        TextField("到着地", text:$arrivalInputText)
+                                    }
+                                }
+                            }
+                            .frame(width: 351, height: 160)
+                            .cornerRadius(8)
+                            ZStack {
+                                Rectangle()
+                                    .fill(Color("BackgroundColor"))
+                                VStack {
+                                    if let unwrappedRoutes = routes {
+                                        ForEach(unwrappedRoutes, id: \.self) { route in
+                                            VStack(alignment: .leading) {
+                                                HStack {
+                                                    Text("\(Int(ceil(route.expectedTravelTime / 60))) min")
+                                                        .font(.title3)
+                                                        .bold()
+                                                    Spacer()
+                                                }
+                                                HStack {
+                                                    VStack {
+                                                        HStack {
+                                                            Text("\(Int(ceil(route.distance / 1.609))) miles")
+                                                            Spacer()
+                                                        }
+//                                                        HStack {
+//                                                            Image(systemName: "football.fill")
+//                                                            Text("Expected points earned")
+//                                                            Text(50)
+//                                                                .font(.title3)
+//                                                                .foregroundStyle(.blue)
+//                                                        }
+                                                    }
+                                                    Button{} label: {
+                                                        ZStack {
+
+                                                            Rectangle()
+                                                                .frame(width: 62, height: 62)
+                                                                .foregroundStyle(.blue)
+                                                                .cornerRadius(8)
+                                                            Text("GO")
+                                                                .foregroundStyle(.white)
+                                                                .font(.title2)
+                                                                .bold()
+                                                        }
+                                                    }
+                                                    .frame(width: 62, height: 62)
+
+                                                }
+                                            }
+                                            .frame(height: 90)
+                                            .padding(15)
+                                        }
+                                    }
+                                    Divider()
+                                }
+                            }
+                            .frame(width: 351, height: 300)
+                            .cornerRadius(8)
+                        }
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: 615)
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(radius: 10)
+                    .offset(y: modalOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { value in
+                                let newOffset = value.translation.height + lastModalOffset
+                                if newOffset < UIScreen.main.bounds.height - 555 {
+                                    modalOffset = UIScreen.main.bounds.height - 555
+                                } else if newOffset > UIScreen.main.bounds.height - 125 {
+                                    modalOffset = UIScreen.main.bounds.height - 125
+                                } else {
+                                    modalOffset = newOffset
+                                }
+                            }
+                            .onEnded { _ in
+                                withAnimation {
+                                    if modalOffset < lastModalOffset {
+                                        modalOffset = UIScreen.main.bounds.height - 555
+                                    } else {
+                                        modalOffset = UIScreen.main.bounds.height - 125
+                                    }
+                                }
+                                lastModalOffset = modalOffset
+                            }
+                    )
                 }
             }
-            .frame(width: 300, height:200)
-            HStack{
-                Text("ルート1")
-                Text("5点獲得できそうだよ")
-            }
-            HStack{
-                Text("ルート2")
-                Text("2点獲得できそうだよ")
-            }
-            Button("このルートで出発"){}
-                .frame(minWidth: 300)
-                .background(Color.cyan)
         }
     }
-
 
     func calculateRoute() async {
         let sourceCoordinate = CLLocationCoordinate2D(
@@ -61,30 +176,19 @@ struct RouteDetailView: View {
         request.source = MKMapItem(placemark: sourcePlacemark)
         request.destination = MKMapItem(placemark: destinationPlacemark)
         request.transportType = .automobile
-        if request.transportType == .transit {
+
+        do {
             let directions = MKDirections(request: request)
-            do {
-                let etaResponse = try await directions.calculateETA()
-                let etaSeconds = etaResponse.expectedTravelTime
-                let etaMinutes = Int(etaSeconds / 60)
-                print("ETA: \(etaMinutes)")
-            } catch {
-                print("ETA Error: \(error.localizedDescription)")
-            }
-        } else {
-            do {
-                let directions = MKDirections(request: request)
-                let response = try await directions.calculate()
-                let routes = response.routes
-                route = routes.first
-                print(routes)
-            } catch {
-                print(error.localizedDescription)
-            }
+            let response = try await directions.calculate()
+            let routes = response.routes
+            self.routes = routes
+            print(routes)
+        } catch {
+            print(error.localizedDescription)
         }
     }
-}
 
+}
 
 #Preview {
     RouteDetailView()
