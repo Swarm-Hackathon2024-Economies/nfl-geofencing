fa7d9f808d926461a452bcaf8684577f81e70c77
taku <takuya_hiraoka@mail.toyota.co.jp>
2024-10-14 18:13:55 +0900

    マイカード画面を作った　これからコンポーネントに分ける

--- nfl-geofencing/Cards/CardsScreen.swift
+++ nfl-geofencing/Cards/CardsScreen.swift
@@ -1,24 +1,105 @@
 import SwiftUI
 
+enum SelectCardsView {
+    case Shop
+    case MyCollection
+}
+
 struct CardsScreen: View {
     @State private var scrollOffset: CGFloat = 0
+    @State private var selectedSection: SelectCardsView = .Shop
     
     var body: some View {
         NavigationStack {
             ScrollView {
                 LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
                     Section {
-                        VStack(spacing: 27) {
-                            NavigationLink {
-                                CardCollectionDetailView()
-                            } label: {
-                                NFLCollectionOverview(title: "NFL Collection", imageName: "CollectionPicture1")
-                                    .foregroundStyle(.black)
+                        if selectedSection == .Shop {
+                            VStack(spacing: 27) {
+                                NavigationLink {
+                                    CardCollectionDetailView()
+                                } label: {
+                                    NFLCollectionOverview(title: "NFL Collection", imageName: "CollectionPicture1")
+                                        .foregroundStyle(.black)
+                                }
+                                NFLCollectionOverview(title: "College Collection", imageName: "CollectionPicture2")
+                                NFLCollectionOverview(title: "Profile Parts", imageName: "CollectionPicture3")
                             }
-                            NFLCollectionOverview(title: "College Collection", imageName: "CollectionPicture2")
-                            NFLCollectionOverview(title: "Profile Parts", imageName: "CollectionPicture3")
+                            .padding(.horizontal)
+                        } else {
+                            VStack {
+                                HStack {
+                                    Text("NFL Collections")
+                                        .font(.title2.bold())
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    HStack{
+                                        Text("23")
+                                            .font(.title2.bold())
+                                        Text("/ 230")
+                                    }
+                                }
+                                HStack {
+                                    NavigationLink {
+                                        PlayerCard()
+                                    } label: {
+                                        Image("MyNFLCollection1")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                            .clipShape(RoundedRectangle(cornerRadius: 8))
+                                            .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                    }
+                                    Image("MyNFLCollection2")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                    Image("MyNFLCollection1")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                }
+                                HStack {
+                                    Spacer()
+                                    Text("View all")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.red)
+                                }
+                                .padding(.bottom, 10)
+                                HStack {
+                                    Text("College Collections")
+                                        .font(.title2.bold())
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    Text("40")
+                                        .font(.title2.bold())
+                                    Text("/ 230")
+                                }
+                                HStack {
+                                    Image("MyCollegeCollection1")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                    Image("MyCollegeCollection2")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                    Image("MyCollegeCollection1")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .clipShape(RoundedRectangle(cornerRadius: 8))
+                                        .shadow(color: .black.opacity(0.3), radius: 5, x: 0, y: 5)
+                                }
+                                HStack {
+                                    Spacer()
+                                    Text("View all")
+                                        .font(.title3.bold())
+                                        .foregroundStyle(.red)
+                                }
+                            }
+                            .padding(.horizontal)
                         }
-                        .padding(.horizontal)
                     } header: {
                         sectionHeader
                     }
@@ -40,17 +121,37 @@ struct CardsScreen: View {
                     .padding(4)
                     .background(Circle().fill(.white))
                 Text("Shop")
-                    .foregroundStyle(.white)
+                    .foregroundStyle(selectedSection == .Shop ? .white : .red)
             }
             .padding([.leading, .top, .bottom], 4)
             .padding(.trailing, 8)
-            .background(Capsule().fill(.red))
+            .background(
+                selectedSection == .Shop ?
+                Capsule().fill(.red)
+                : Capsule().fill(.white)
+            )
+            .background(
+                Capsule().stroke(.red)
+            )
+            .onTapGesture {
+                selectedSection = .Shop
+            }
             
             Text("My Collection")
-                .foregroundStyle(.red)
+                .foregroundStyle(selectedSection == .MyCollection ? .white : .red)
                 .padding(.horizontal, 8)
                 .padding(.vertical, 4)
-                .background(Capsule().stroke(.red))
+                .background(
+                    selectedSection == .MyCollection ?
+                    Capsule().fill(.red)
+                    : Capsule().fill(.white)
+                )
+                .background(
+                    Capsule().stroke(.red)
+                )
+                .onTapGesture {
+                    selectedSection = .MyCollection
+                }
             Spacer()
         }
         .padding(.horizontal)
