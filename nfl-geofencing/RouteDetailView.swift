777ebc298048f2acf6b4f7e422665c0569c8f426
Keisuke-Isaji <sajiemon.999@gmail.com>
2024-10-14 22:43:29 +0900

    RouteDetailViewのUI調整とリファクタリング

--- nfl-geofencing/RouteDetailView.swift
+++ nfl-geofencing/RouteDetailView.swift
@@ -8,11 +8,19 @@ struct RouteDetailView: View {
     @State private var selectedRoute: MKRoute?
     @State private var dangerArea: [CircleArea] = []
     let dangerAreaRepository: DangerAreaRepository = JsonDangerAreaRepository()
-
+    
     @State private var dangerPointCountList: [Int] = []
     @State private var rankList: [Int] = []
     @State private var scoreList: [Int] = []
-
+    @State private var sourceCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(
+        latitude: 33.08575588060863,
+        longitude: -96.83922557513722
+    )
+    @State private var destinationCoordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(
+        latitude: 32.74816795373609,
+        longitude: -97.09333068671008
+    )
+    
     var body: some View {
         ZStack(alignment: .bottom) {
             Map {
@@ -31,7 +39,7 @@ struct RouteDetailView: View {
             .onAppear {
                 Task {
                     dangerArea = dangerAreaRepository.getAll()
-                    await calculateRoute()
+                    await calculateRoute(source: self.sourceCoordinate, destination: self.destinationCoordinate)
                     for route in routes {
                         dangerPointCountList.append(countRouteAvoidsArea(route: route))
                         self.rankList = convertPointToRank(originalArray: dangerPointCountList)
@@ -50,7 +58,7 @@ struct RouteDetailView: View {
                             Spacer()
                         }
                         .padding(EdgeInsets(
-                            top: 0, leading: 22, bottom: 0, trailing: 0
+                            top: 20, leading: 22, bottom: 0, trailing: 0
                         ))
                         VStack {
                             ZStack {
@@ -128,63 +136,63 @@ struct RouteDetailView: View {
                                 ZStack(alignment: .topLeading) {
                                     Rectangle()
                                         .fill(Color("BackgroundColor"))
-                                    VStack {
-
-                                        ForEach(routes.indices, id: \.self) { index in
-                                            let route = routes[index]
-                                            VStack(alignment: .leading, spacing: 0) {
-                                                Text("\(Int(ceil(route.expectedTravelTime / 60))) min")
-                                                    .font(.title3)
-                                                    .bold()
-                                                    .padding(EdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0))
-                                                HStack {
-                                                    VStack(alignment: .leading, spacing: 13) {
-                                                        HStack {
-                                                            Text("\(Int(ceil(route.distance / 1609))) miles")
-                                                            Spacer()
-                                                            Text("\(countRouteAvoidsArea(route: route).description)")
-                                                                .font(.caption2)
-                                                            Text("SafetyRank:\(rankList[index])")
-                                                                .font(.headline)
+                                    if routes.count == 0 {
+                                        Text("Now Loading...(いい感じのGIF当てたい!)")
+                                    } else {
+                                        VStack {
+                                            ForEach(routes.indices, id: \.self) { index in
+                                                let route = routes[index]
+                                                VStack(alignment: .leading, spacing: 0) {
+                                                    Text("\(Int(ceil(route.expectedTravelTime / 60))) min")
+                                                        .font(.title3)
+                                                        .bold()
+                                                        .padding(EdgeInsets(top: 16, leading: 0, bottom: 0, trailing: 0))
+                                                    HStack {
+                                                        VStack(alignment: .leading, spacing: 13) {
+                                                            HStack {
+                                                                Text("\(Int(ceil(route.distance / 1609))) miles")
+                                                                Spacer()
+                                                                Text("\(countRouteAvoidsArea(route: route).description)")
+                                                                    .font(.caption2)
+                                                                Text("SafetyRank:\(rankList[index])")
+                                                                    .font(.headline)
+                                                            }
+                                                            HStack(spacing: 8) {
+                                                                Image(systemName: "football.fill")
+                                                                    .foregroundStyle(.blue)
+                                                                Text("Expected points earned")
+                                                                Text("\(scoreList[index])")
+                                                                    .font(.title3)
+                                                                    .bold()
+                                                                    .foregroundStyle(.blue)
+                                                            }
                                                         }
-                                                        HStack(spacing: 8) {
-                                                            Image(systemName: "football.fill")
-                                                                .foregroundStyle(.blue)
-                                                            Text("Expected points earned")
-                                                            Text("\(scoreList[index])")
-                                                                .font(.title3)
-                                                                .bold()
-                                                                .foregroundStyle(.blue)
+                                                        Button{
+                                                            self.selectedRoute = route
+                                                        } label: {
+                                                            ZStack {
+                                                                Rectangle()
+                                                                    .frame(width: 62, height: 62)
+                                                                    .foregroundStyle(.blue)
+                                                                    .cornerRadius(8)
+                                                                Text("GO")
+                                                                    .foregroundStyle(.white)
+                                                                    .font(.title2)
+                                                                    .bold()
+                                                            }
                                                         }
+                                                        .frame(width: 62, height: 62)
                                                     }
-                                                    Button{
-                                                        self.selectedRoute = route
-                                                    } label: {
-                                                        ZStack {
-                                                            Rectangle()
-                                                                .frame(width: 62, height: 62)
-                                                                .foregroundStyle(.blue)
-                                                                .cornerRadius(8)
-                                                            Text("GO")
-                                                                .foregroundStyle(.white)
-                                                                .font(.title2)
-                                                                .bold()
-                                                        }
-                                                    }
-                                                    .frame(width: 62, height: 62)
                                                 }
+                                                .frame(height: 90)
+                                                Divider()
+                                                    .padding(EdgeInsets(top: 12, leading: 0, bottom: 0, trailing: 0))
                                             }
-                                            .frame(height: 90)
-                                            //                                            .onAppear {
-                                            //                                                dangerPointCountList.insert(countRouteAvoidsArea(route: route), at: 0)
-                                            //                                            }
-                                            Divider()
-                                                .padding(EdgeInsets(top: 12, leading: 0, bottom: 0, trailing: 0))
                                         }
+                                        .padding(EdgeInsets(top: 10, leading: 15, bottom: 0, trailing: 15))
                                     }
-                                    .padding(EdgeInsets(top: 0, leading: 15, bottom: 0, trailing: 15))
                                 }
-                                .frame(width: 351, height: 300)
+                                .frame(width: 351, height: 600)
                                 .cornerRadius(8)
                             }
                         }
@@ -196,26 +204,17 @@ struct RouteDetailView: View {
             }
         }
     }
-
-    func calculateRoute() async {
-        let sourceCoordinate = CLLocationCoordinate2D(
-            latitude: 33.08575588060863,
-            longitude: -96.83922557513722
-        )
-        let destinationCoordinate = CLLocationCoordinate2D(
-            latitude: 32.74816795373609,
-            longitude: -97.09333068671008
-        )
-
-        let sourcePlacemark = MKPlacemark(coordinate: sourceCoordinate)
-        let destinationPlacemark = MKPlacemark(coordinate: destinationCoordinate)
-
+    
+    func calculateRoute(source: CLLocationCoordinate2D, destination: CLLocationCoordinate2D) async {
+        let sourcePlacemark = MKPlacemark(coordinate: source)
+        let destinationPlacemark = MKPlacemark(coordinate: destination)
+        
         let request = MKDirections.Request()
         request.requestsAlternateRoutes = true
         request.source = MKMapItem(placemark: sourcePlacemark)
         request.destination = MKMapItem(placemark: destinationPlacemark)
         request.transportType = .automobile
-
+        
         do {
             let directions = MKDirections(request: request)
             let response = try await directions.calculate()
@@ -225,42 +224,42 @@ struct RouteDetailView: View {
             print(error.localizedDescription)
         }
     }
-
+    
     func countRouteAvoidsArea(route: MKRoute) -> Int {
         let routePolyline = route.polyline
         let pointCount = routePolyline.pointCount
         let points = routePolyline.points()
         var count = 0
-
+        
         for i in 0..<pointCount {
             let coordinate = points[i].coordinate
             let location = CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)
-
+            
             func roundToThirdDecimalPlace(value: Double) -> Double {
                 return (value * 1000).rounded() / 1000
             }
-
+            
             func isCoordinateInRoundedRoute(route: CLLocationCoordinate2D, testCoordinate: CLLocationCoordinate2D) -> Bool {
                 let roundedTestCoordinate = CLLocationCoordinate2D(
                     latitude: roundToThirdDecimalPlace(value: testCoordinate.latitude),
                     longitude: roundToThirdDecimalPlace(value: testCoordinate.longitude)
                 )
-
+                
                 let roundedRouteCoordinate = CLLocationCoordinate2D(
                     latitude: roundToThirdDecimalPlace(value: route.latitude),
                     longitude: roundToThirdDecimalPlace(value: route.longitude)
                 )
-
+                
                 if roundedRouteCoordinate.latitude == roundedTestCoordinate.latitude &&
                     roundedRouteCoordinate.longitude == roundedTestCoordinate.longitude {
                     return true
                 }
                 return false
             }
-
+            
             for dangerPoint in dangerArea {
                 let coordinate = CLLocationCoordinate2D(latitude: dangerPoint.latitude, longitude: dangerPoint.longitude)
-
+                
                 if isCoordinateInRoundedRoute(route: coordinate, testCoordinate: coordinate) {
                     count += 1
                 }
@@ -268,20 +267,19 @@ struct RouteDetailView: View {
         }
         return count
     }
-
+    
     func generateCoordinatePatterns(center: CLLocationCoordinate2D, latitudeRange: ClosedRange<Double>, longitudeRange: ClosedRange<Double>) -> [CLLocationCoordinate2D] {
         var patterns: [CLLocationCoordinate2D] = []
-
-        let latStep = 0.0001
-        let lonStep = 0.0001
-
-        for lat in stride(from: latitudeRange.lowerBound, through: latitudeRange.upperBound, by: latStep) {
-            for lon in stride(from: longitudeRange.lowerBound, through: longitudeRange.upperBound, by: lonStep) {
+        
+        let step = 0.0001
+        
+        for lat in stride(from: latitudeRange.lowerBound, through: latitudeRange.upperBound, by: step) {
+            for lon in stride(from: longitudeRange.lowerBound, through: longitudeRange.upperBound, by: step) {
                 let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: lon)
                 patterns.append(coordinate)
             }
         }
-
+        
         return patterns
     }
 }
@@ -289,31 +287,31 @@ struct RouteDetailView: View {
 func convertPointToRank(originalArray: [Int]) -> [Int] {
     let sortedArray = originalArray.sorted()
     var ranks = [Int](repeating: 0, count: originalArray.count)
-
+    
     for (index, value) in originalArray.enumerated() {
         if let rank = sortedArray.firstIndex(of: value) {
             ranks[index] = rank + 1
         }
     }
-
+    
     return ranks
 }
 
 func assignScores(from array: [Int]) -> [Int] {
     let count = array.count
     if count < 2 { return [] }
-
+    
     var scores = [Int](repeating: 0, count: count)
-
+    
     let indexedArray = array.enumerated().map { (index: $0.offset, value: $0.element) }
-
+    
     let sortedArray = indexedArray.sorted { $0.value < $1.value }
-
+    
     for (rank, element) in sortedArray.enumerated() {
         let score = 80 - ((70 * rank) / (count - 1))
         scores[element.index] = score
     }
-
+    
     return scores
 }
 
