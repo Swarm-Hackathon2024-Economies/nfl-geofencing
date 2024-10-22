a0259d52a0762279a88af30c344ec6fff298c905
shiyoohashi <alicehecate666@gmail.com>
2024-10-21 20:43:15 -0500

    add:Collage and FlagFootball in Team

--- nfl-geofencing/Models/Team.swift
+++ nfl-geofencing/Models/Team.swift
@@ -22,3 +22,49 @@ struct Team: Equatable {
         Team(rank: "12", name: "Commanders", points: "144,861", icon: "washington-commanders", color: .indigo),
     ]
 }
+
+struct collageTeam: Equatable {
+    let rank: String
+    let name: String
+    let points: String
+    let icon: String
+    let color: Color
+
+    static let allItems: [Team] = [
+        Team(rank: "1", name: "Rat", points: "945,108", icon: "baltimore-ravens", color: .red),
+        Team(rank: "2", name: "Ox", points: "927,582", icon: "buffalo-bills", color: .blue),
+        Team(rank: "3", name: "Tiger", points: "873,451", icon: "cincinnati-bengals", color: .black),
+        Team(rank: "4", name: "Rabbit", points: "712,481", icon: "indianapolis-colts", color: .brown),
+        Team(rank: "5", name: "Dragon", points: "654,124", icon: "new-england-patriots", color: .yellow),
+        Team(rank: "6", name: "Snake", points: "569,030", icon: "new-los-angeles-rams", color: .cyan),
+        Team(rank: "7", name: "Horse", points: "489,205", icon: "new-orleans-saints", color: .mint),
+        Team(rank: "8", name: "Goat", points: "332,479", icon: "philadelphia-eagles", color: .green),
+        Team(rank: "9", name: "Monkey", points: "224,597", icon: "san-diego-chargers", color: .pink),
+        Team(rank: "10", name: "Rooster", points: "187,656", icon: "san-francisco-49ers", color: .purple),
+        Team(rank: "11", name: "Dog", points: "148,933", icon: "seattle-seahawks", color: .teal),
+        Team(rank: "12", name: "Pig", points: "110,022", icon: "washington-commanders", color: .indigo),
+    ]
+}
+
+struct flagFootballTeam: Equatable {
+    let rank: String
+    let name: String
+    let points: String
+    let icon: String
+    let color: Color
+
+    static let allItems: [Team] = [
+        Team(rank: "1", name: "Taiyang", points: "978,341", icon: "baltimore-ravens", color: .red),
+        Team(rank: "2", name: "Taiyin", points: "952,108", icon: "buffalo-bills", color: .blue),
+        Team(rank: "3", name: "Tianji", points: "849,755", icon: "cincinnati-bengals", color: .black),
+        Team(rank: "4", name: "Taisui", points: "794,503", icon: "indianapolis-colts", color: .brown),
+        Team(rank: "5", name: "Tianliang", points: "682,320", icon: "new-england-patriots", color: .yellow),
+        Team(rank: "6", name: "Tianyao",points: "633,109",icon: "new-los-angeles-rams", color: .cyan),
+        Team(rank: "7", name: "Tianfu", points: "549,201", icon: "new-orleans-saints", color: .mint),
+        Team(rank: "8", name: "Tianxiang", points: "467,892", icon: "philadelphia-eagles", color: .green),
+        Team(rank: "9", name: "Tianzun",points: "341,255", icon: "san-diego-chargers", color: .pink),
+        Team(rank: "10", name: "Tiankui" ,points: "279,872", icon: "san-francisco-49ers", color: .purple),
+        Team(rank: "11", name: "Tianxing", points: "198,543", icon: "seattle-seahawks", color: .teal),
+        Team(rank: "12", name: "Tianyao", points: "123,076", icon: "washington-commanders", color: .indigo),
+    ]
+}
--- nfl-geofencing/Views/iPhone/Home/RankingView.swift
+++ nfl-geofencing/Views/iPhone/Home/RankingView.swift
@@ -1,36 +1,148 @@
 import SwiftUI
-
+enum SelectTeamView {
+    case NFL
+    case Collage
+    case FlagFootball
+    
+}
 struct RankingView: View {
+    @State private var scrollOffset: CGFloat = 0
+    @State private var selectedSection: SelectTeamView = .NFL
+    
     var body: some View {
         VStack {
             Text("Ranking")
                 .font(.title.bold())
                 .frame(maxWidth: .infinity, alignment: .leading)
-            HStack {
-                CapsuleButton("NFL", style: .filled)
-                CapsuleButton("Collage")
-                CapsuleButton("Flag FootBall")
-                Spacer()
-            }
-            Grid {
-                ForEach(Team.allItems, id: \.rank) { team in
-                    GridRow {
-                        Text(team.rank)
-                            .font(.title.bold())
-                            .foregroundStyle(.indigo)
-                        Image(team.icon)
-                            .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(maxWidth: 46, maxHeight: 32)
-                        
-                        Text(team.name)
-                            .font(.title2.bold())
-                            .frame(maxWidth: .infinity, alignment: .leading)
-                        Text(team.points)
+            LazyVStack(spacing: 0, pinnedViews: .sectionHeaders) {
+                Section {
+                    if selectedSection == .NFL {
+                        Grid {
+                            ForEach(Team.allItems, id: \.rank) { team in
+                                GridRow {
+                                    Text(team.rank)
+                                        .font(.title.bold())
+                                        .foregroundStyle(.indigo)
+                                    Image(team.icon)
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .frame(maxWidth: 46, maxHeight: 32)
+                                    Text(team.name)
+                                        .font(.title2.bold())
+                                        .frame(maxWidth: .infinity, alignment: .leading)
+                                    Text(team.points)
+                                }
+                            }
+                        }
+                        .padding(.horizontal)
+                    } else if selectedSection == .Collage {                                Grid {
+                        ForEach(collageTeam.allItems, id: \.rank) { team in
+                            GridRow {
+                                Text(team.rank)
+                                    .font(.title.bold())
+                                    .foregroundStyle(.indigo)
+                                Image(team.icon)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 46, maxHeight: 32)
+                                
+                                Text(team.name)
+                                    .font(.title2.bold())
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text(team.points)
+                            }
+                        }
                     }
+                    .padding(.horizontal)
+                    } else  {                                Grid {
+                        ForEach(flagFootballTeam.allItems, id: \.rank) { team in
+                            GridRow {
+                                Text(team.rank)
+                                    .font(.title.bold())
+                                    .foregroundStyle(.indigo)
+                                Image(team.icon)
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                    .frame(maxWidth: 46, maxHeight: 32)
+                                
+                                Text(team.name)
+                                    .font(.title2.bold())
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text(team.points)
+                            }
+                        }
+                    }
+                    .padding(.horizontal)
+                    }
+                } header: {
+                    sectionHeader
                 }
             }
+            .scrollSpy("ScrollView", scrollOffset: $scrollOffset)
+            
         }
+    }
+    var sectionHeader: some View {
+        HStack {
+            HStack {
+                Image(systemName: "football.fill")
+                    .foregroundStyle(.red)
+                    .font(.caption)
+                    .padding(4)
+                    .background(Circle().fill(.white))
+                Text("NFL")
+                    .foregroundStyle(selectedSection == .NFL ? .white : .red)
+            }
+            .padding([.leading, .top, .bottom], 4)
+            .padding(.trailing, 8)
+            .background(
+                selectedSection == .NFL ?
+                Capsule().fill(.red)
+                : Capsule().fill(.white)
+            )
+            .background(
+                Capsule().stroke(.red)
+            )
+            .onTapGesture {
+                selectedSection = .NFL
+            }
+            
+            Text("Collage")
+                .foregroundStyle(selectedSection == .Collage ? .white : .red)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(
+                    selectedSection == .Collage ?
+                    Capsule().fill(.red)
+                    : Capsule().fill(.white)
+                )
+                .background(
+                    Capsule().stroke(.red)
+                )
+                .onTapGesture {
+                    selectedSection = .Collage
+                }
+            Text("FlagFootball")
+                .foregroundStyle(selectedSection == .FlagFootball ? .white : .red)
+                .padding(.horizontal, 8)
+                .padding(.vertical, 4)
+                .background(
+                    selectedSection == .FlagFootball ?
+                    Capsule().fill(.red)
+                    : Capsule().fill(.white)
+                )
+                .background(
+                    Capsule().stroke(.red)
+                )
+                .onTapGesture {
+                    selectedSection = .FlagFootball
+                }
+
+            Spacer()
+        }
+        .padding(.horizontal)
+        .padding(.vertical, 8)
+        .background(.thickMaterial.opacity(scrollOffset < 0 ? 1 : 0))
     }
 }
 
