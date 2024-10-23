b041c3c0b3e38a3d14471e8af887cdee4785807b
shiyoohashi <alicehecate666@gmail.com>
2024-10-22 23:32:42 -0500

    add:title animation in LoginView

--- nfl-geofencing/Views/iPhone/LoginView.swift
+++ nfl-geofencing/Views/iPhone/LoginView.swift
@@ -13,7 +13,10 @@ struct LoginView: View {
     let onFinish: () -> Void
     @State private var loginInfo = LoginInfo.blank
     @State private var isPresented = false
-    
+    @State private var isAnimating = false
+    @State private var offsetY: CGFloat = -500
+    @State private var offsetX: CGFloat = -700
+    @State private var isBouncing = false
     var body: some View {
         NavigationStack {
             VStack {
@@ -22,9 +25,16 @@ struct LoginView: View {
                     .resizable()
                     .aspectRatio(contentMode: .fit)
                     .frame(width: 294, height: 111)
+                    .offset(x: offsetX, y: offsetY)
+                    .rotationEffect(.degrees(isAnimating ? 5 : 0))
+                    .onAppear {
+                        dropAnimation()
+                    }
                 Spacer()
                 Button {
-                    // TOYOTA LOGIN PAGE
+                    if let url = URL(string: "https://id.toyota/") {
+                        UIApplication.shared.open(url)
+                    }
                 } label: {
                     ZStack {
                         Image("TOYOTA_logo")
@@ -99,6 +109,61 @@ struct LoginView: View {
             .padding(30)
             .navigationDestination(isPresented: $isPresented) {
                 SetupProfileView(isShowSetUpProfile: $isPresented)
+            }
+        }
+    }
+    private func dropAnimation() {
+        withAnimation(.easeIn(duration: 1.0)) {
+            offsetY = 0
+            offsetX = -50
+        }
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            bounceAnimation()
+        }
+    }
+    
+    private func bounceAnimation() {
+        let bounceCount = 4
+        for i in 0..<bounceCount {
+            withAnimation(.interpolatingSpring(stiffness: 50,  damping: 5)) {
+                if bounceCount == 0 {
+                    offsetY -= 20
+                    offsetX += 10
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {print("a")
+                        withAnimation {
+                            offsetY += 20
+                            offsetX += 10
+                        }
+                    }
+                } else if i == 1 {
+                    offsetY -= 15
+                    offsetX += 7
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation {
+                            offsetY += 15
+                            offsetX += 7
+                        }
+                    }
+                }
+                else{
+                    offsetY -= 10
+                    offsetX += 5
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        withAnimation {
+                            offsetY += 10
+                            offsetX += 5
+                        }
+                    }
+                    if i == bounceCount - 1 {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            withAnimation(.linear(duration: 0.3).delay(1)) {
+                                isAnimating.toggle()
+                            }
+                        }
+                    }
+                }
             }
         }
     }
