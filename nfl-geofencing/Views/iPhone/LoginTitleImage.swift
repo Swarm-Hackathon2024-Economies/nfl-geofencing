b410abca46d7992cc0a790544baa12989f3c3411
shiyoohashi <alicehecate666@gmail.com>
2024-10-23 00:24:40 -0500

    change:refactor in LoginView

---
+++ nfl-geofencing/Views/iPhone/LoginTitleImage.swift
@@ -0,0 +1,99 @@
+//
+//  LoginTitleImage.swift
+//  nfl-geofencing
+//
+//  Created by shiyo_ohashi on 2024/10/23.
+//
+
+import SwiftUI
+
+struct LoginTitleImage: View {
+    @State private var isAnimating = false
+    @State private var offsetY: CGFloat = -500
+    @State private var offsetX: CGFloat = -700
+    @State private var isBouncing = false
+    @State private var isShaking = false
+    @State private var shakeCount = 0
+    var body: some View {
+        Image("TitleIcon")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 294, height: 111)
+                            .offset(x: offsetX, y: offsetY)
+                            .rotationEffect(.degrees(isAnimating ? 5 : 0))
+                            .onAppear {
+                                dropAnimation()
+                            }
+                            .rotationEffect(.degrees(isShaking ? 12 : 0))
+                                        .onTapGesture {
+                                            withAnimation(Animation.default.repeatCount(3, autoreverses: true)) {
+                                                isShaking.toggle()
+                                            }
+                                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                                withAnimation {
+                                                    isShaking = false
+                                                }
+                                            }
+                                        }
+    }
+    private func dropAnimation() {
+        withAnimation(.easeIn(duration: 1.0)) {
+            offsetY = 0
+            offsetX = -50
+        }
+        
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+            bounceAnimation()
+        }
+    }
+    
+    private func bounceAnimation() {
+        let bounceCount = 4
+        for i in 0..<bounceCount {
+            withAnimation(.interpolatingSpring(stiffness: 50,  damping: 5)) {
+                if bounceCount == 0 {
+                    offsetY -= 20
+                    offsetX += 10
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {print("a")
+                        withAnimation {
+                            offsetY += 20
+                            offsetX += 10
+                        }
+                    }
+                } else if i == 1 {
+                    offsetY -= 15
+                    offsetX += 7
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                        withAnimation {
+                            offsetY += 15
+                            offsetX += 7
+                        }
+                    }
+                }
+                else{
+                    offsetY -= 10
+                    offsetX += 5
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                        withAnimation {
+                            offsetY += 10
+                            offsetX += 5
+                        }
+                    }
+                    if i == bounceCount - 1 {
+                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                            withAnimation(.linear(duration: 0.3).delay(1)) {
+                                isAnimating.toggle()
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+#Preview {
+    LoginTitleImage()
+}
--- nfl-geofencing/Views/iPhone/LoginView.swift
+++ nfl-geofencing/Views/iPhone/LoginView.swift
@@ -13,23 +13,11 @@ struct LoginView: View {
     let onFinish: () -> Void
     @State private var loginInfo = LoginInfo.blank
     @State private var isPresented = false
-    @State private var isAnimating = false
-    @State private var offsetY: CGFloat = -500
-    @State private var offsetX: CGFloat = -700
-    @State private var isBouncing = false
     var body: some View {
         NavigationStack {
             VStack {
                 Spacer()
-                Image("TitleIcon")
-                    .resizable()
-                    .aspectRatio(contentMode: .fit)
-                    .frame(width: 294, height: 111)
-                    .offset(x: offsetX, y: offsetY)
-                    .rotationEffect(.degrees(isAnimating ? 5 : 0))
-                    .onAppear {
-                        dropAnimation()
-                    }
+                LoginTitleImage()
                 Spacer()
                 Button {
                     if let url = URL(string: "https://id.toyota/") {
@@ -109,61 +97,6 @@ struct LoginView: View {
             .padding(30)
             .navigationDestination(isPresented: $isPresented) {
                 SetupProfileView(isShowSetUpProfile: $isPresented)
-            }
-        }
-    }
-    private func dropAnimation() {
-        withAnimation(.easeIn(duration: 1.0)) {
-            offsetY = 0
-            offsetX = -50
-        }
-        
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            bounceAnimation()
-        }
-    }
-    
-    private func bounceAnimation() {
-        let bounceCount = 4
-        for i in 0..<bounceCount {
-            withAnimation(.interpolatingSpring(stiffness: 50,  damping: 5)) {
-                if bounceCount == 0 {
-                    offsetY -= 20
-                    offsetX += 10
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {print("a")
-                        withAnimation {
-                            offsetY += 20
-                            offsetX += 10
-                        }
-                    }
-                } else if i == 1 {
-                    offsetY -= 15
-                    offsetX += 7
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                        withAnimation {
-                            offsetY += 15
-                            offsetX += 7
-                        }
-                    }
-                }
-                else{
-                    offsetY -= 10
-                    offsetX += 5
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                        withAnimation {
-                            offsetY += 10
-                            offsetX += 5
-                        }
-                    }
-                    if i == bounceCount - 1 {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
-                            withAnimation(.linear(duration: 0.3).delay(1)) {
-                                isAnimating.toggle()
-                            }
-                        }
-                    }
-                }
             }
         }
     }
